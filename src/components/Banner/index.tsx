afee47681faaad6d37df92788653876b31894e5c
pabloliveeira <pablo230197@gmail.com>
2024-11-04 22:23:44 -0300

    consertando

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,7 +3,11 @@ import { Background, Category, Name } from './styles'
 import restaurantBg from '../../assets/images/restaurant-bg.png'
 
 const Banner = () => (
-  <Background style={{ backgroundImage: `url(${restaurantBg})` }}>
+  <Background
+    style={{
+      backgroundImage: `url(${restaurantBg})`
+    }}
+  >
     <div className="container">
       <Category>Italiana</Category>
       <Name>La Dolce Vita Trattoria</Name>
--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -5,6 +5,23 @@ export const Background = styled.div`
   width: 100%;
   height: 280px;
   display: block;
+
+  position: relative;
+
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100%;
+
+  &::after {
+    position: absolute;
+    background-color: #000;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    content: '';
+    opacity: 0.5;
+  }
 `
 export const Name = styled.h2`
   font-size: 32px;
